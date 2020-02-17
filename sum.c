1ab98434da9d49a85516ddac301edf4b5f8e677b
svvsprasad <61155255+svvsprasad@users.noreply.github.com>
2020-02-17 20:59:51 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter Any Three Digits To Find Sum Of The Given Numbers a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+printf("sum of %d+%d+%d is %d",a,b,c,a+b+c);
+return 0;
+}
+
