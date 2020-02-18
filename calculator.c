09d493958f8293599f050a54edbbde896ed05ed1
svvsprasad <61155255+svvsprasad@users.noreply.github.com>
2020-02-18 19:50:25 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+ int a,b;
+ printf("Enter Two values");
+ scanf("%d%d",&a,&b);
+ printf("The Sum Of %d And %d is %d\n",a,b,a+b);
+ printf("The difference Of %d And %d is %d\n",a,b,a-b);
+ printf("The multiplication Of %d And %d is %d\n",a,b,a*b);
+ printf("The dividend Of %d And %d is %d\n",a,b,a/b);
+ printf("The remainder Of %d And %d is %d\n",a,b,a%b);
+
+
+}
